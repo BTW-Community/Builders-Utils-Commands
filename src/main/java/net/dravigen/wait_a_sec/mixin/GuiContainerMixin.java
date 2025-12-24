ea5ff8b42e05df7edf0d3f5dd2ae1ad5b8030f7b
DraViGen <fireshadoxe@gmail.com>
2025-12-25 00:55:37 +0100

    fixes not being able to enchant and other "fonctional inventory" like brewing stand, trade

--- src/main/java/net/dravigen/wait_a_sec/mixin/GuiContainerMixin.java
+++ src/main/java/net/dravigen/wait_a_sec/mixin/GuiContainerMixin.java
@@ -1,6 +1,10 @@
 package net.dravigen.wait_a_sec.mixin;
 
-import net.minecraft.src.GuiContainer;
+import btw.client.gui.HamperGui;
+import btw.client.gui.HopperGui;
+import btw.client.gui.InfernalEnchanterGui;
+import btw.client.gui.RenameItemGui;
+import net.minecraft.src.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +15,14 @@ public abstract class GuiContainerMixin {
 	
 	@Inject(method = "doesGuiPauseGame", at = @At("RETURN"), cancellable = true)
 	private void pauseTheGame(CallbackInfoReturnable<Boolean> cir) {
+		GuiContainer gui = (GuiContainer) (Object) this;
+		
+		if (gui instanceof GuiBrewingStand ||
+				gui instanceof GuiEnchantment ||
+				gui instanceof InfernalEnchanterGui ||
+				gui instanceof GuiMerchant ||
+				gui instanceof GuiRepair) return;
+		
 		cir.setReturnValue(true);
 	}
 }
